3c93fcdabda5055613560f54fab0e98eea5dcd6f
Rahul Salunkhe <rsrahulsalunkhe4@gmail.com>
2024-08-12 21:29:34 +0530

    .

--- src/components/newsCard/NewsCard.jsx
+++ src/components/newsCard/NewsCard.jsx
@@ -1,7 +1,6 @@
 import React, {useState} from 'react'
 import './style.scss'
 import cub from './../../assets/cub.svg'
-import fire from './../../assets/fire.svg'
 import opn from './../../assets/opn.svg'
 import banner from './../../assets/banner.jpeg'
 import banner2 from './../../assets/banner2.jpeg'
@@ -17,15 +16,22 @@ const NewsCard = (props) => {
         <div className='news-card'>
             <img src={banner} alt="" />
             <div className='card-detail-mobile' onClick={() => navigate(`/detail/${props.categorie}`)}>
-                <p className='news-card-date'>13 March 2021</p>
-                <h1 className='my-2' style={{lineHeight: 1.15}}>Maize trade offer Aboti canvassing Jalgaon</h1>
-                <p className='sub-title my-2' style={{lineHeight: 1.15}}>Deals in Maize, Wheat, Moong, Chana Urad, Jowar, Kabuli</p>
+                <span className='date mb-2'>27-09-2023</span>
+                <h1 className='cart-title' style={{lineHeight: 1.15}}>Maize trade offer Aboti canvassing Jalgaon</h1>
+                <p className='sub-title' style={{lineHeight: 1.15}}>Deals in Maize, Wheat, Moong, Chana Urad, Jowar, Kabuli</p>
+                <div className='divider1 my-4'>
+                    <div className='gradient-background1'></div>
+                    <div className='gradient-dot me-2'></div>
+                    <span>HIGHLIGHTS</span>
+                    <div className='gradient-dot ms-2'></div>
+                    <div className='gradient-background2'></div>
+                </div>
                 <div style={{lineHeight: 1.15}}>
-                <div className='flex mt-2'>
-                    <div className='w-[7%] h-[24px]'>
-                        <img className='w-full h-full' src={opn} alt="" />
-                    </div>
-                    <p className='w-[94%] text-left ms-1' style={{color: 'var(--black)', fontWeight: 700, lineHeight: 1.15}}>Trading Activity Very Dull in Chana and rate down</p>
+                    <div className='flex mt-2'>
+                        <div className='w-[7%] h-[24px]'>
+                            <img className='w-full h-full' src={opn} alt="" />
+                        </div>
+                        <p className='w-[94%] text-left ms-1' style={{color: 'var(--black)', fontWeight: 700, lineHeight: 1.15}}>Trading Activity Very Dull in Chana and rate down</p>
                     </div>
                     <div className='flex mt-2'>
                         <div className='w-[7%] h-[24px]'>
@@ -34,28 +40,35 @@ const NewsCard = (props) => {
                         <p className='w-[94%] text-left ms-1' style={{color: 'var(--black)', fontWeight: 700, lineHeight: 1.15}}>Trading Activity Very Dull in Chana and rate down</p>
                     </div>
                 </div>
-                {/* <p className='load-more text-right me-2' onClick={() => navigate(`/detail/${props.categorie}`)}><i>Load More Reports {'>'}</i></p> */}
+                    {/* <p className='load-more text-right me-2' onClick={() => navigate(`/detail/${props.categorie}`)}><i>Load More Reports {'>'}</i></p> */}
                 <p className='load-more text-right me-2'><i>Load More Reports {'>'}</i></p>
             </div>
 
             <div className='card-detail' onClick={() => {setShow(true);setVideoId(video.key);}}>
-                <p className='news-card-date'>13 March 2021</p>
-                <h1 className='my-2' style={{lineHeight: 1.15}}>Maize trade offer Aboti canvassing Jalgaon</h1>
+                <span className='date'>13 March 2021</span>
+                <h1 className='cart-title' style={{lineHeight: 1.15}}>Maize trade offer Aboti canvassing Jalgaon</h1>
                 <p className='sub-title my-2' style={{lineHeight: 1.15}}>Deals in Maize, Wheat, Moong, Chana Urad, Jowar, Kabuli</p>
+                <div className='divider1 my-4'>
+                    <div className='gradient-background1'></div>
+                    <div className='gradient-dot me-2'></div>
+                    <span>HIGHLIGHTS</span>
+                    <div className='gradient-dot ms-2'></div>
+                    <div className='gradient-background2'></div>
+                </div>
                 <div style={{lineHeight: 1.15}}>
-                <div className='flex mt-2'>
-                    <div className='w-[7%] h-[24px]'>
-                        <img className='w-full h-full' src={opn} alt="" />
-                    </div>
-                    <p className='w-[94%] text-left ms-1' style={{color: 'var(--black)', fontWeight: 700, lineHeight: 1.15}}>Trading Activity Very Dull in Chana and rate down</p>
-                    </div>
                     <div className='flex mt-2'>
                         <div className='w-[7%] h-[24px]'>
-                            <img className='w-full h-full' src={cub} alt="" />
+                            <img className='w-full h-full' src={opn} alt="" />
                         </div>
                         <p className='w-[94%] text-left ms-1' style={{color: 'var(--black)', fontWeight: 700, lineHeight: 1.15}}>Trading Activity Very Dull in Chana and rate down</p>
+                        </div>
+                        <div className='flex mt-2'>
+                            <div className='w-[7%] h-[24px]'>
+                                <img className='w-full h-full' src={cub} alt="" />
+                            </div>
+                            <p className='w-[94%] text-left ms-1' style={{color: 'var(--black)', fontWeight: 700, lineHeight: 1.15}}>Trading Activity Very Dull in Chana and rate down</p>
+                        </div>
                     </div>
-                </div>
                 {/* <p className='load-more text-right me-2' onClick={() => navigate(`/detail/${props.categorie}`)}><i>Load More Reports {'>'}</i></p> */}
                 <p className='load-more text-right me-2'><i>Load More Reports {'>'}</i></p>
             </div>
